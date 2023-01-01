d98d24b86ee0a7bdd4439f98df6d504392d38397
Abel Aebker <github@ujikon.com>
2023-01-01 01:45:41 -0500

    set vimtex concealing settings
    add markdown support with pandoc

--- user/init.lua
+++ user/init.lua
@@ -249,8 +249,34 @@ local config = {
                     vim.g.vimtex_view_method = 'skim'
                     vim.g.vimtex_view_skim_sync = 1
                     vim.g.vimtex_view_skim_activate = 1
+
+                    vim.g.vimtex_syntax_conceal = {
+                        ["math_super_sub"] = 0,
+                        ["accents"] = 1,
+                        ["greek"] = 1,
+                        ["styles"] = 0,
+                        ["math_fracs"] = 0,
+                        ["math_symbols"] = 0,
+                        ["spacing"] = 1,
+                        ["ligatures"] = 1,
+                        ["fancy"] = 1,
+                        ["sections"] = 0,
+                        ["math_delimiters"] = 1,
+                        ["math_bounds"] = 1,
+                        ["cites"] = 1
+                    }
                 end,
-                -- ft = 'tex'
+            },
+            ["vim-pandoc/vim-pandoc"] = {},
+            ["vim-pandoc/vim-pandoc-syntax"] = {},
+            ["iamcco/markdown-preview.nvim"] = {
+                run = function()
+                    vim.fn["mkdp#util#install"]()
+                end,
+                config = function()
+                    vim.keymap.set("n", "<LocalLeader>rr", ":MarkdownPreview<CR>")
+                end,
+                ft = { "markdown" }
             },
             ["L3MON4D3/LuaSnip"] = {
                 config = function()
