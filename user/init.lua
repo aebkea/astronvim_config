2f062151bb7e617fd092efd46bb7c868d5771766
Abel Aebker <github@ujikon.com>
2023-01-01 01:44:04 -0500

    fix more stupid indenting

--- user/init.lua
+++ user/init.lua
@@ -1,6 +1,6 @@
---              AstroNvim Configuration Table
+-- AstroNvim Configuration Table
 -- All configuration changes should go inside of the table below
-
+--
 -- You can think of a Lua "table" as a dictionary like data structure the
 -- normal format is "key = value". These also handle array like data structures
 -- where a value with no key simply has an implicit numeric key
