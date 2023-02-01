068daa1439166db5e2b6d33966f6cee8109f2f5a
Abel Aebker <github@ujikon.com>
2023-02-01 00:02:35 -0500

    set relativenumbers and breakindent true

--- user/init.lua
+++ user/init.lua
@@ -43,11 +43,12 @@ local config = {
     options = {
         opt = {
             -- set to true or false etc.
-            relativenumber = false, -- sets vim.opt.relativenumber
+            relativenumber = true, -- sets vim.opt.relativenumber
             number = true, -- sets vim.opt.number
             spell = false, -- sets vim.opt.spell
             signcolumn = "auto", -- sets vim.opt.signcolumn to auto
             wrap = false, -- sets vim.opt.wrap
+            breakindent = true,
             tabstop = 4,
             shiftwidth = 4,
         },
